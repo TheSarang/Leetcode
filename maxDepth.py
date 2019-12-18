6c8b31606a4554dc672fa36c61ffd79f2d8a3c2e
Sarang Narkhede <sn7330@rit.edu>
2019-12-18 13:25:58 -0500

    559. Maximum Depth of N-ary Tree

---
+++ maxDepth.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+from queue import Queue
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root == None:
+            return 0
+        q1 = Queue()
+        q2 = Queue()
+        q1.put(root)
+        count=0
+        while not q1.empty():
+            curr = q1.get()
+            for i in self.successor(curr):
+                q2.put(i) 
+            if q1.empty():
+                q1 = q2
+                q2 = Queue()
+                count+=1
+                
+        return count
+    def successor(self,node):
+        
+        return node.children
+        
