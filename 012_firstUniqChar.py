62c10f7a5b666ab2ea0c262fb15be55fbb283ae5
Sarang Narkhede <sn7330@rit.edu>
2020-03-26 23:15:52 -0400

    387. First Unique Character in a String

--- 012_firstUniqChar.py
+++ 012_firstUniqChar.py
@@ -6,7 +6,7 @@ class Solution:
                 dict1[i] = 1
             else:
                 dict1[i] += 1
-
+ 
         for (i, v) in dict1.items():
             if v == 1:
                 return s.find(i)
