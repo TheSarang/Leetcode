e6823a90756090b675f4ba012c52468380671e6b
Sarang Narkhede <sn7330@rit.edu>
2020-09-01 12:47:12 -0400

    12. Integer to Roman

---
+++ 085_intToRoman.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        result = ""
+        for i, v in enumerate(values):
+            result += num//values[i]*numerals[i]
+            num %= values[i]
+        return result 
