d05e0b43f093404aef8b2ba559d22f2c8ef059bc
Sarang Narkhede <sn7330@rit.edu>
2021-05-10 16:45:29 -0400

    253. Meeting Rooms II

--- 109_minMeetingRooms.py
+++ 109_minMeetingRooms.py
@@ -1,5 +1,19 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        count = 1
+        intervals.sort(key = lambda x: x[0])
+        heap = []
+        heapq.heappush(heap, intervals[0][1])
+        for i in range(1, len(intervals)):
+            pair = intervals[i]
+            if pair[0] >= heap[0]:
+                heapq.heappushpop(heap, pair[1])
+            else:
+                heapq.heappush(heap, pair[1])                
+        return len(heap)
+    
+#     ----------------------------------------------------------
+    
         starting_times = sorted([intervals[i][0] for i in range(len(intervals))])
         end_times = sorted([intervals[i][1] for i in range(len(intervals))])
         L = len(intervals)
@@ -11,4 +25,3 @@ class Solution:
             used_rooms += 1
             start_point += 1
         return used_rooms
-        
