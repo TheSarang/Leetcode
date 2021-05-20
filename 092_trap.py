06572f3253d4e1c7a29bf5c07533647f029c6999
Sarang Narkhede <sn7330@rit.edu>
2021-05-20 11:36:15 -0400

    42. Trapping Rain Water

--- 092_trap.py
+++ 092_trap.py
@@ -1,5 +1,32 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        
+        # space complexity -> O(1)
+        # time complexity -> O(3n)
+        max_h_idx = 0
+        max_h = 0
+        res = 0
+        for i in range(len(height)):
+            if height[i] > max_h:
+                max_h = max(height[i], max_h)
+                max_h_idx = i
+                
+        max_h_left = 0
+        for i in range(max_h_idx):
+            max_h_left = max(max_h_left, height[i])
+            res += max_h_left - height[i]
+        
+        max_h_right = 0
+        for i in range(len(height)-1, max_h_idx, -1):
+            max_h_right = max(max_h_right, height[i])
+            res += max_h_right - height[i]
+            
+        return res
+    
+#     -----------------------------------------------------------------
+        
+        # space complexity -> O(2n)
+        # time complexity -> O(3n)
         if height == []:
             return 0
         left_array = [0 for _ in range(len(height))]
