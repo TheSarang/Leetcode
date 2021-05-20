d70c63e1924f78256a0f7e83a9b64921ad580f86
Sarang Narkhede <sn7330@rit.edu>
2021-05-20 14:07:49 -0400

    200. Number of Islands

--- 035_numIslands.py
+++ 035_numIslands.py
@@ -1,18 +1,38 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(grid, i,j):
-            if (0<=i<len(grid)) and (0<=j<len(grid[i])) and (grid[i][j] == '1'):
-                grid[i][j] = '#'
-                dfs(grid, i+1,j)
-                dfs(grid, i,j+1)
-                dfs(grid, i-1,j)
-                dfs(grid, i,j-1)
-            
-        islands = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == '1':
-                    dfs(grid, i,j)
-                    islands += 1
+        res = 0
+        n = len(grid)
+        m = len(grid[0])
         
-        return islands
+        def dfs(i, j):
+            if 0<=i<n and 0<=j<m and grid[i][j] == '1':
+                grid[i][j] = 'X'
+                dfs(i+1, j)
+                dfs(i-1, j)
+                dfs(i, j+1)
+                dfs(i, j-1)
+                
+        
+        def bfs(i, j):
+            q1 = deque()  
+            q1.append((i,j))
+            while q1:
+                dx, dy = q1.popleft() 
+                directions = [[1,0],[0,1],[-1,0],[0,-1]]
+                for (x, y) in directions:
+                    xx = x + dx 
+                    yy = y + dy
+                    if 0<=xx<n and 0<=yy<m and grid[xx][yy] == '1':
+                        q1.append((xx,yy))
+                        grid[xx][yy] = 'X'
+                        
+                        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    # dfs(i, j)
+                    bfs(i, j)
+                    res+=1
+
+        return res
