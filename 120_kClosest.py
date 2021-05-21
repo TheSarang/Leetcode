5f954ba008398f6d743487739e90e6d5dd6c4541
Sarang Narkhede <sn7330@rit.edu>
2021-05-21 15:42:19 -0400

    973. K Closest Points to Origin

--- 120_kClosest.py
+++ 120_kClosest.py
@@ -2,6 +2,7 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
         # TC = O(N*logk)
+        # SC = O(K)
         for (x, y) in points:
             dist = -(math.pow((0-x),2) + math.pow((0-y),2))
             if len(heap) == k:
@@ -10,3 +11,33 @@ class Solution:
                 heapq.heappush(heap, (dist, x, y))
         
         return [(x,y) for (dist, x, y) in heap]
+  
+#     ------------------------------------------------------------------------------------------------------------------------------------------------
+
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+        # TC = O(N*logN)
+        # SC = O(N)
+        for x,y in points:
+            distance = math.sqrt((0-x)**2 + (0-y)**2)
+            heapq.heappush(heap, (distance, (x,y)))
+        return [pair[1] for pair in heapq.nsmallest(k, heap, key= lambda x:x[0])]
+        
+#     ------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:    
+        dict_ = {}
+        res = []
+        # TC = O(N)+ O(N*logN) + O(K)
+        # SC = O(N)
+        for x,y in points:
+            distance = math.sqrt((0-x)**2 + (0-y)**2)
+            dict_[(x,y)] = distance
+        
+        dict_sorted = sorted(dict_.items(), key = lambda kv:(kv[1])) 
+        for i in range(k):
+            res.append(dict_sorted[i][0])
+        
+        return res
