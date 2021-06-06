0600124e31d1814fd47494946cca0890b1012810
Sarang Narkhede <sn7330@rit.edu>
2021-06-06 15:40:55 -0400

    updated version

--- 043_reorderLogFiles.py
+++ 043_reorderLogFiles.py
@@ -2,13 +2,12 @@ class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         letter_logs = []
         digit_logs = []
-        for i in logs:
-            if i.split(" ")[1].isdigit():
-                digit_logs.append(i)
+        
+        for log in logs:
+            if log.split(' ')[1].isdigit():
+                digit_logs.append(log)
             else:
-                letter_logs.append(i)
-                
-        letter_logs.sort(key = lambda x:x.split(" ")[0])
-        letter_logs.sort(key = lambda x:x.split(" ")[1:])
-        letter_logs.extend(digit_logs)
-        return letter_logs
+                letter_logs.append(log)      
+        letter_logs.sort(key = lambda x: (x.split()[1:], x.split()[0]))
+        res = letter_logs + digit_logs
+        return res
