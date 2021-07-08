8566e44c09738a6b04012116f6eb241b57628841
Sarang Narkhede <sn7330@rit.edu>
2021-07-07 21:43:56 -0400

    new approach

--- 087_kClosest.py
+++ 087_kClosest.py
@@ -1,5 +1,33 @@
 import math
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        return sorted(points, key=lambda x:math.sqrt(x[0]*x[0] + x[1]*x[1]))[:K]
+        '''
+        BF
+        
+        1] sort an array based on distance return k values
+    
+        
+        
+        '''
+        
+        # points.sort(key=lambda x: (x[0]+x[0])**2 + (x[1]+x[1])**2)
+        # return points[:k]
+    
+    
+        '''
+        heaps
+        1] create max heap
+        2] put elemtns k time 
+        3] pop from heap if len(heap) > k then push new value and then pop it
+        
+        '''
+        heap = []
+        for pair in points:
+            x = pair[0]
+            y = pair[1]
+            distance = x*x + y*y
+            heapq.heappush(heap, (-distance, (x,y)))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return (pair[1] for pair in heap)
                 
