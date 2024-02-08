c0b44f2776850ae400f5f4eeee97add6d70dbd5b
Sarang Narkhede <narkhedesarang@gmail.com>
2024-02-07 21:21:49 -0800

    Update 074_groupAnagrams.py
    Add another way of solving groupAnagrams

--- 074_groupAnagrams.py
+++ 074_groupAnagrams.py
@@ -8,7 +8,7 @@ class Solution:
             else:
                 dicts[key].append(word)
         return (dicts.values())
-
+------------------------------------------------------------
    
 import collections
 class Solution:
@@ -17,3 +17,29 @@ class Solution:
         for val in strs:
             dict_[tuple(sorted(val))].append(val)
         return dict_.values()
+
+------------------------------------------------------------
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]::
+        res = {}
+        for s in strs:
+            word = self._helper(s)
+            if word not in res:
+                res[word] = [s]
+            else:
+                res[word].append(s)
+
+        return res.values()
+        
+    def _helper(self, word):
+        alpha_array = [0] * 26
+        res = ""
+        for c in word:
+            alpha_array[ord(c) - ord("a")] += 1
+        
+        for i in range(len(alpha_array)):
+            if alpha_array[i] != 0:
+                res += str(chr(ord("a") + i)) + str(alpha_array[i])
+        
+        return res
