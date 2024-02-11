84ebbc6a95cc1a6dae9be4ae4babb0844d0bd094
Sarang Narkhede <narkhedesarang@gmail.com>
2024-02-10 20:21:44 -0800

    217. Contains Duplicate

---
+++ 134_containsDuplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+        for num in nums:
+            if num in seen and seen[num] >= 1:
+                return True
+            seen[num] = seen.get(num, 0) + 1
+        return False
