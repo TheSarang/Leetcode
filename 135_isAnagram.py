693b7cd3e0ae090100a3535a3234418f806ca9d2
Sarang Narkhede <narkhedesarang@gmail.com>
2024-02-11 23:45:03 -0800

    Create 135_isAnagram.py
    242. Valid Anagram

---
+++ 135_isAnagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        res = dict()
+      
+        for alpha in s:
+            if alpha in res:
+                res[alpha] += 1
+            else:
+                res[alpha] = 1
+
+        for alpha in t:
+            if alpha in res:
+                res[alpha] -= 1
+            else:
+                return False
+
+        for vals in res.values():
+            if vals != 0:
+                return False
+              
+        return True
+        
+        
+
