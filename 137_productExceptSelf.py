730a8ad0ebfbecea2499aebda805502f1122e6cd
Sarang Narkhede <narkhedesarang@gmail.com>
2024-02-15 22:02:42 -0800

    Create 137_productExceptSelf.py
    238. Product of Array Except Self

---
+++ 137_productExceptSelf.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+
+        n = len(nums)
+        prefix_product = 1
+        postfix_product = 1
+        result = [0]*n
+        for i in range(n):
+            result[i] = prefix_product
+            prefix_product *= nums[i]
+        for i in range(n-1,-1,-1):
+            result[i] *= postfix_product
+            postfix_product *= nums[i]
+        return result
