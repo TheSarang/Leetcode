f655f236f32979ef3fddcd9fd59f668870ff4cf5
Sarang Narkhede <narkhedesarang@gmail.com>
2024-02-16 23:29:54 -0800

    Create 138_threeSum.py
    15. 3Sum

---
+++ 138_threeSum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        nums.sort()
+        ans = []
+        mySet = set()
+
+        for i in range(n):
+            j = i + 1
+            k = n - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total == 0:
+                    temp = [nums[i], nums[j], nums[k]]
+                    mySet.add(tuple(temp))
+                    j += 1
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                elif total > 0:
+                    k -= 1
+
+        for x in mySet:
+            ans.append(list(x))
+        return ans
