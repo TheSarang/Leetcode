397367c3810b65cdd57e43c9cebb3f550d636748
Sarang Narkhede <narkhedesarang@gmail.com>
2024-05-25 21:51:29 -0700

    Update 137_productExceptSelf.py

--- 137_productExceptSelf.py
+++ 137_productExceptSelf.py
@@ -3,15 +3,18 @@ class Solution:
 
         # Time complexity: O(n)
         # Space complexity: O(n)
-
+    
         n = len(nums)
-        prefix_product = 1
-        postfix_product = 1
         result = [0]*n
+        
+        prefix_product = 1
         for i in range(n):
             result[i] = prefix_product
             prefix_product *= nums[i]
+
+        postfix_product = 1
         for i in range(n-1,-1,-1):
             result[i] *= postfix_product
             postfix_product *= nums[i]
+            
         return result
