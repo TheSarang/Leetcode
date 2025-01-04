be6373b7102d974ef632ada0860ce05bb1a8e7cf
Sarang Narkhede <narkhedesarang@gmail.com>
2025-01-03 20:48:03 -0800

    Update 112_maxSubArray.py
    Add complexities

--- 112_maxSubArray.py
+++ 112_maxSubArray.py
@@ -1,8 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #kadanes algo
+        
+        # Kadane's Algo
+        # TC: O(n)
+        # SC: O(1)
+        
         LM = GM = nums[0]
         for i in range(1, len(nums)):
             LM = max(nums[i], nums[i] + LM)
             GM = max(GM, LM)
+            
         return GM
